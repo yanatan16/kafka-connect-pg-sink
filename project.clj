44f37757f4dc3a485dc279df1700f03e47097251
Jon Eisen <jon@joneisen.me>
2016-10-28 13:08:04 -0600

    Version 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kafka-connect-pg-sink "0.1.2-SNAPSHOT"
+(defproject kafka-connect-pg-sink "0.1.2"
   :description "Kafka Connector Sink to push records to postgres"
   :url "https://github.com/yanatan16/kafka-connect-pg-sink"
   :license {:name "MIT"
